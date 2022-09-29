01738dcc18d69748ad9d766a1a18d3526cc6561a
Marian Petrov <war1an91@gmail.com>
2022-09-29 13:21:56 +0300

    cache

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require('next-pwa/cache')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true
-  },
-  i18n: {
-    locales: ['en', 'bg'],
-    defaultLocale: 'en'
-  },
-  pwa: {
-    dest: "public"
-  }
+    reactStrictMode: false,
+    compiler: {
+        // ssr and displayName are configured by default
+        styledComponents: true
+    },
+    i18n: {
+        locales: ['en', 'bg'],
+        defaultLocale: 'en'
+    },
+    pwa: {
+        dest: "public",
+        runtimeCaching
+    }
 }
 
 module.exports = withPWA(nextConfig)
